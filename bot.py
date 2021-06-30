f1d82946b7f5a8bf9c86e5d6bf2661802b7ac951
razmanov666 <razmanov666@gmail.com>
2021-06-30 17:50:31 +0300

    feat: update status

--- bot.py
+++ bot.py
@@ -8,15 +8,22 @@ bot = telebot.TeleBot("1827098555:AAFay8gZy4c5pyxUc6I3HpkOgeheDl63Tww")
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
     bot.send_message(message.chat.id, text="Ура, я заработал")
-    with open('users.json', 'r') as read_users:
+    try:
+        read_data = get_users(message)
+        bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню!')
+        print('exists')
+    except FileNotFoundError:
+        create_user(message)
+        read_data = get_users(message)
+    update_user_status('NEW_STATUS', message)
+    task_for_user(message)
+
+
+def get_users(message):
+    with open('users/' + str(message.chat.id) + '.json') as read_users:
         read_data = json.load(read_users)
         read_users.close()
-
-    if len(read_data) >= 1:
-        result = json.dumps(read_data)
-        result = re.sub(r'}$', ',', result)
-        create_user(result, read_data, message)
-    task_for_user(message)
+    return read_data
 
 
 def task_for_user(message):
@@ -27,44 +34,41 @@ def task_for_user(message):
     bot.send_message(message.chat.id, text="Сколько будет 150 + 150?", reply_markup=markup)
 
 
-def create_user(result, read_data, message):
-    # bot.send_message(message.chat.id, text="Ура, я заработал")
-    # print((result + '"user' + str(len(read_data) + 1) + '": [{"chat_id": "' + str(message.chat.id) + '",' +
-    #                        '"username": "' + message.from_user.username + '",' +
-    #                        '"first_name": "' + message.from_user.first_name + '",' +
-    #                        '"last_name": "' + message.from_user.last_name + '",' +
-    #                        '"status": "reg12142"}]}'))
-    parse = check_user_in_json(read_data, message)
-    user_data = get_user_data(result, read_data, message)
-    if parse:
-        print('parse: OK')
-        with open('users.json', 'w', encoding='utf-8') as users:
+def create_user(message):
+    user_data = get_user_data(message)
+    print('parse: OK')
+    write_user_data(user_data, message)
+    update_user_status('NEW_STATUS', message)
+
+
+def write_user_data(user_data, message):
+    try:
+        with open('users/' + str(message.chat.id)+'.json', 'x', encoding='utf-8') as users:
             json.dump(user_data, users, ensure_ascii=False, indent=4)
-    else:
-        bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню!')
+    except FileExistsError:
+        with open('users/' + str(message.chat.id)+'.json', 'w', encoding='utf-8') as users:
+            json.dump(user_data, users, ensure_ascii=False, indent=4)
+    finally:
+        print('OK')
 
 
-def check_user_in_json(read_data, message):
-    parse = True
-    user_list = range(1, len(read_data) + 1)
-    for i in user_list:
-        for user in read_data["user" + str(i)]:
-            for k, v in user.items():
-                if k == 'chat_id' and v == str(message.chat.id):
-                    if message.from_user.username:
-                        print('parse: already exist' + '\nUsername: ' + message.from_user.username)
-                    elif message.from_user.last_name:
-                        name = message.from_user.first_name + message.from_user.last_name
-                        print('parse: already exist' + '\nName: ' + name)
-                    else:
-                        print('parse: already exist' + '\nName: ' + message.from_user.first_name)
-                    print('K = ' + str(message.chat.id))
-                    parse = False
-                    break
-    return parse
+# def check_user_in_json(read_data, message):
+#     try:
+#         reading_data = read_data[str(message.chat.id)]
+#         if message.from_user.username:
+#             print('parse: already exist' + '\nUsername: ' + message.from_user.username)
+#         elif message.from_user.last_name:
+#             name = message.from_user.first_name + message.from_user.last_name
+#             print('parse: already exist' + '\nName: ' + name)
+#         else:
+#             print('parse: already exist' + '\nName: ' + message.from_user.first_name)
+#             print('K = ' + str(message.chat.id))
+#         return False
+#     except KeyError:
+#         return True
 
 
-def get_user_data(result, read_data, message):
+def get_user_data(message):
     if message.from_user.last_name:
         last_name = message.from_user.last_name
     else:
@@ -73,13 +77,20 @@ def get_user_data(result, read_data, message):
         username = message.from_user.username
     else:
         username = ''
-    user_data = (result + '"user' + str(len(read_data) + 1) + '": [{"chat_id": "' + str(message.chat.id) + '",' +
+    user_data = ('{"' + str(message.chat.id) + '": { ' +
                  '"username": "' + username + '",' +
                  '"first_name": "' + message.from_user.first_name + '",' +
                  '"last_name": "' + last_name + '",' +
-                 '"status": "reg12142"}]}')
+                 '"status": "reg12142"}}')
 
     return json.loads(user_data)
+
+
+def update_user_status(status, message):
+    read_data = get_users(message)
+    new_user_data = read_data
+    new_user_data[str(message.chat.id)]['status'] = status
+    write_user_data(new_user_data, message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -88,7 +99,7 @@ def query_handler(call):
     if call.data == '300':
         answer = 'Ну, ты сам знаешь кто твой любовник...'
     else:
-        answer = 'Плохо, пробуй еще! @Drow_Ranger'
+        answer = 'Плохо, пробуй еще!'
     bot.send_message(call.message.chat.id, answer)
 
 
