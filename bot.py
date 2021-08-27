7a0a7882512c44383e50bc3b4efb3e2aba6e0ebb
Alexey Razmanov <razmanov666>
2021-08-27 17:48:50 +0300

    working without status

--- bot.py
+++ bot.py
@@ -10,16 +10,19 @@ bot = telebot.TeleBot("1827098555:AAFay8gZy4c5pyxUc6I3HpkOgeheDl63Tww")
 
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
-    collection.update_one({'username': ''}, {'$set': {'username': 'None'}})
     bot.send_message(message.chat.id, text="Ура, я заработал")
     userdb = create_userdb(message)
     save_user_data(message, userdb)
     show_usersdb(collection)
+    questions(message)
+
+
+def questions(message):
     task_for_user(message)
 
 
 def save_user_data(message, userdb):
-    if collection.count_documents({'chat_id': message.chat.id}) != 0:
+    if collection.count_documents({'chat_id': message.chat.id}) > 0:
         bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню!')
         update_userdb(message, userdb)
     else:
@@ -51,7 +54,7 @@ def create_userdb(message):
         'username': username,
         'first_name': first_name,
         'last_name': last_name,
-        'status': 'new_reg'
+        'status': status
     }
     return userdb
 
@@ -71,7 +74,14 @@ def show_usersdb(show_collection):
                                             + ' ' + user['last_name'] + ' ' + user['status'])
 
 
-@bot.callback_query_handler(func=lambda call: True)
+def ask_user(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Да', callback_data='1'))
+    markup.add(telebot.types.InlineKeyboardButton(text='Нет', callback_data='0'))
+    bot.send_message(message.chat.id, text="Хочешь попробовать еще раз?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: int(call.data) > 1)
 def query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id, text='Ответ принят!')
     if call.data == '300':
@@ -79,6 +89,21 @@ def query_handler(call):
     else:
         answer = 'Плохо, пробуй еще! @Drow_Ranger'
     bot.send_message(call.message.chat.id, answer)
+    ask_user(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('1'))
+def callback_worker_age(call):
+    bot.answer_callback_query(callback_query_id=call.id, text='ОКЕЙ!')
+    task_for_user(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('0'))
+def callback_worker_age(call):
+    answer = 'Ладно, в следующий раз!'
+    bot.send_message(call.message.chat.id, answer)
+    save_user_data(call.message, create_userdb(call.message))
+    bot.stop_bot()
 
 
 bot.polling()
