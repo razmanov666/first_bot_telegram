382cc64d3dce43abf144cfd6609f2b768e69ca4d
Alexey Razmanov <razmanov666>
2021-09-07 13:57:25 +0300

    send Rostislav

--- bot.py
+++ bot.py
@@ -6,32 +6,28 @@ import config
 db_client = pymongo.MongoClient("mongodb://localhost:27017/")
 current_db = db_client["razmanov_admin"]
 collection = current_db["users"]
-
 bot = telebot.TeleBot(config.token)
-
 
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
+    status = 'no answer'
     bot.send_message(message.chat.id, text="Ура, я заработал")
-    userdb = create_userdb(message)
+    userdb = create_userdb(message, status)
     save_user_data(message, userdb)
     # show_usersdb(collection)
     task_for_user(message)
 
 
 def save_user_data(message, userdb):
-    print('collection = ')
-    print(collection.count_documents({}))
-
     if collection.count_documents({'chat_id': message.chat.id}) > 0:
-        print(1)
         if not message.from_user.username:
-            bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню!')
-            update_userdb(message, userdb)
+            if collection.find_one({'chat_id': message.chat.id, 'status': 'True result'}):
+                bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню! Последний раз ответил')
+                update_userdb(message, userdb)
         elif message.from_user.username != 'first_telegram66345_bot':
-            print(2)
-            bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню!')
-            update_userdb(message, userdb)
+            if collection.find_one({'chat_id': message.chat.id, 'status': 'True result'}):
+                bot.send_message(message.chat.id, text='Привет, ' + message.from_user.first_name + ', я тебя помню! Последний раз ответил')
+                update_userdb(message, userdb)
     else:
         insert_result = collection.insert_one(userdb)
         print('New user: \n')
@@ -43,7 +39,7 @@ def update_userdb(message, userdb):
     collection.update_one({'chat_id': message.chat.id}, {'$set': userdb})
 
 
-def create_userdb(message):
+def create_userdb(message, status):
     if not message.from_user.username:
         username = 'None'
     else:
@@ -62,7 +58,7 @@ def create_userdb(message):
         'username': username,
         'first_name': first_name,
         'last_name': last_name,
-        'status': 'status'
+        'status': status
     }
     return userdb
 
@@ -94,23 +90,25 @@ def query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id, text='Ответ принят!')
     if call.data == '300':
         answer = 'Ну, ты сам знаешь кто твой любовник...'
+        status = 'True result'
     else:
         answer = 'Плохо, пробуй еще! @Drow_Ranger'
+        status = 'False result'
+    save_user_data(call.message, create_userdb(call.message, status))
     bot.send_message(call.message.chat.id, answer)
     ask_user(call.message)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('1'))
-def callback_worker_age(call):
+def callback_repeat(call):
     bot.answer_callback_query(callback_query_id=call.id, text='ОКЕЙ!')
     task_for_user(call.message)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('0'))
-def callback_worker_age(call):
+def callback_repeat(call):
     answer = 'Ладно, в следующий раз!'
     bot.send_message(call.message.chat.id, answer)
-    save_user_data(call.message, create_userdb(call.message))
     
 
 
